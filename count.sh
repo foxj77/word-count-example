9d0740a60a2f63b1e78b36ba9ded01ca210c95fa
John Fox <foxj77@gmail.com>
2024-05-11 15:21:52 +0100

    FIX: handling apostrophes and adding special characters to data file

--- count.sh
+++ count.sh
@@ -10,10 +10,10 @@ if [ ! -f "$1" ]; then
 fi
 
 # Get the total word count
-total_words=$(cat "$1" | tr -s ' ' '\n' | wc -l)
+total_words=$(cat "$1" | grep -oE '\w+'\''?\w*' | wc -l)
 
 # Get the total unique word count
-total_unique_words=$(cat "$1" | tr -s ' ' '\n' | tr 'A-Z' 'a-z' | sort | uniq | wc -l)
+total_unique_words=$(cat "$1" | grep -oE '\w+'\''?\w*' | tr 'A-Z' 'a-z' | sort | uniq | wc -l)
 
 # Print the filename and total word count
 echo "File: $1"
@@ -25,4 +25,4 @@ printf "%-5s %-25s %s\n" "No." "Word" "Occurrences"
 
 # Process the file to count unique words and their occurrences, sorted by frequency
 # Add 'sed -e '/^$/d'' to remove blank lines
-cat "$1" | tr -sc 'A-Za-z' '\n' | sed -e '/^$/d' | tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | awk '{printf "%-5s %-25s %s\n", NR".", $2, $1}'
+cat "$1" | grep -oE '\w+'\''?\w*' | tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | awk '{printf "%-5s %-25s %s\n", NR".", $2, $1}'
