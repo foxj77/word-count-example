dd240dace231c37df087a7f854c3c19f3ffa20c0
John Fox <foxj77@gmail.com>
2024-05-11 14:41:30 +0100

    FEAT: Aligning count column

--- count.sh
+++ count.sh
@@ -12,5 +12,8 @@ if [ ! -f "$1" ]; then
   exit 1
 fi
 
+# Print the header using printf for alignment
+printf "%-25s %s\n" "Word" "Occurrences"
+
 # Process the file to count unique words and their occurrences, sorted by frequency
-cat "$1" | tr -sc 'A-Za-z' '\n' | tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | awk '{printf "%-20s - %s\n", $2, $1}'
+cat "$1" | tr -sc 'A-Za-z' '\n' | tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | awk '{printf "%-25s %s\n", $2, $1}'
